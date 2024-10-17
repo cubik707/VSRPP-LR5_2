5528f9ae0bcbfb6614420db0c5cc86b63959b7e1
Vladislava <stdemidovez@gmail.com>
2024-10-17 16:47:00 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LR5_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Select = new System.Windows.Forms.Button();
+            this.SelectAll = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(402, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(47, 192);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(402, 164);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Select
+            // 
+            this.Select.Location = new System.Drawing.Point(591, 85);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(105, 23);
+            this.Select.TabIndex = 2;
+            this.Select.Text = "Select";
+            this.Select.UseVisualStyleBackColor = true;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // SelectAll
+            // 
+            this.SelectAll.Location = new System.Drawing.Point(591, 127);
+            this.SelectAll.Name = "SelectAll";
+            this.SelectAll.Size = new System.Drawing.Size(105, 23);
+            this.SelectAll.TabIndex = 3;
+            this.SelectAll.Text = "Select All";
+            this.SelectAll.UseVisualStyleBackColor = true;
+            this.SelectAll.Click += new System.EventHandler(this.SelectAll_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(591, 214);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(105, 23);
+            this.Update.TabIndex = 4;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(591, 170);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(105, 23);
+            this.Insert.TabIndex = 5;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(591, 256);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(105, 23);
+            this.Delete.TabIndex = 6;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.SelectAll);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Select;
+        private System.Windows.Forms.Button SelectAll;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR5_2
+{
+    public partial class Form1 : Form
+    {
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Books.mdb;";
+        private OleDbConnection myConnection;
+
+        private void makeSelectFromDB(string query)
+        {
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            OleDbDataReader reader = command.ExecuteReader();
+            listBox1.Items.Clear();
+            while (reader.Read())
+            {
+                listBox1.Items.Add(reader[0].ToString() + " " + reader[1].ToString() + " "
+                    + reader[2].ToString() + " " + reader[3].ToString());
+            }
+            reader.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+        }
+
+        private void Select_Click(object sender, EventArgs e)
+        {
+            string element = listBox1.SelectedItem.ToString();
+            element = element.Substring(element.IndexOf(' '));
+            textBox1.Text = element;
+            string query = "SELECT book_id, title, author, genre FROM Books ORDER BY book_id";
+            makeSelectFromDB(query);
+        }
+
+        private void SelectAll_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT book_id, title, author, genre FROM Books ORDER BY book_id";
+            makeSelectFromDB(query);
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            string element = textBox1.Text.Trim();
+
+            string[] words = element.Split(new char[] { ' ' });
+            string Title = words[0];
+            string Author = words[1];
+            string Genre = words[2];
+
+            string query = "INSERT INTO Books (title, author, genre) VALUES ('" + Title + "', '" + Author + "', '" + Genre + "')";
+
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+
+            string query1 = "SELECT book_id, title, author, genre FROM Books ORDER BY book_id";
+            makeSelectFromDB(query1);
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            string element = listBox1.SelectedItem.ToString();
+            string id = element.Remove(element.IndexOf(" "));
+
+            element = textBox1.Text.Trim();
+
+            string[] words = element.Split(new char[] { ' ' });
+            string Title = words[0];
+            string Author = words[1];
+            string Genre = words[2];
+
+            string query = "UPDATE Books SET title = '" + Title + "', author = '" + Author + "', genre = '" + Genre + "' WHERE book_id = " + id;
+
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+
+            string query1 = "SELECT book_id, title, author, genre FROM Books ORDER BY book_id";
+            makeSelectFromDB(query1);
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            string element = listBox1.SelectedItem.ToString();
+            element = element.Remove(element.IndexOf(' '));
+            string query = "DELETE FROM Books WHERE book_id = " + element;
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.ExecuteNonQuery();
+            string query1 = "SELECT book_id, title, author, genre FROM Books ORDER BY book_id";
+            makeSelectFromDB(query1);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            myConnection.Close();
+        }
+    }
+}
